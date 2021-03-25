98c18456862db8b5b056a9c53cdae0704d2f9f31
João Vitor Santos <joaooviitorr@hotmail.com>
2021-03-25 08:28:19 -0300

    Updated searchs methods in clinical register controller

--- src/controllers/ClinicalRegister.ts
+++ src/controllers/ClinicalRegister.ts
@@ -26,6 +26,60 @@ class ClinicalRegisterController {
 				return response.json(clinicalRegisterView.previousRegisters(registers));
 			}
 
+			if (patientId) {
+				const registers = await repository
+					.createQueryBuilder('rc')
+					.select([
+						'rc.id as id',
+						'p.id as patientId',
+						'p.name as patientName',
+						'rc.date as date',
+						'rc.time as time',
+						'COUNT(rc.id) AS amount',
+					])
+					.innerJoin('rc.patient', 'p')
+					.where(`p.id = ${patientId} `)
+					.groupBy('p.name')
+					.getRawMany();
+				return response.json(clinicalRegisterView.listAll(registers));
+			}
+
+			if (patientName) {
+				const registers = await repository
+					.createQueryBuilder('rc')
+					.select([
+						'rc.id as id',
+						'p.id as patientId',
+						'p.name as patientName',
+						'rc.date as date',
+						'rc.time as time',
+						'COUNT(rc.id) AS amount',
+					])
+					.innerJoin('rc.patient', 'p')
+					.where(`p.name LIKE '%${patientName}%' `)
+					.groupBy('p.name')
+					.getRawMany();
+				return response.json(clinicalRegisterView.listAll(registers));
+			}
+
+			if (date) {
+				const registers = await repository
+					.createQueryBuilder('rc')
+					.select([
+						'rc.id as id',
+						'p.id as patientId',
+						'p.name as patientName',
+						'rc.date as date',
+						'rc.time as time',
+						'COUNT(rc.id) AS amount',
+					])
+					.innerJoin('rc.patient', 'p')
+					.where(`rc.date = '${date}' `)
+					.groupBy('p.name')
+					.getRawMany();
+				return response.json(clinicalRegisterView.listAll(registers));
+			}
+
 			const registers = await repository.query(
 				'(SELECT p.name as patientName, p.id as patientId, r.id, r.date,  r.time,COUNT(*) AS amount ' +
 					'FROM clinical_register r INNER JOIN patient p ON r.patient_id = p.id ' +
@@ -37,7 +91,8 @@ class ClinicalRegisterController {
 			);
 
 			return response.json(clinicalRegisterView.listAll(registers));
-		} catch {
+		} catch (error) {
+			console.log(error);
 			return response
 				.status(500)
 				.json({ message: 'Error when try list registers' });
